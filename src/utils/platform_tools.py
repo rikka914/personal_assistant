a6db9579e3f47c13a44740b68cde2f5fd3813e99
Rika <769689085@qq.com>
2026-03-19 10:45:19 +0800

    update platform_tools

--- src/utils/platform_tools.py
+++ src/utils/platform_tools.py
@@ -16,14 +16,35 @@ def get_paltform_info():
         "is_mac":system=="Darwin",
     }
 
-def get_config_path(filename):
+def get_config_path(config_type):
     """根据平台返回配置路径"""
     plat_info = get_paltform_info()
     config_dir = os.path.join(os.path.dirname(__file__),"..","..","config")
 
     if plat_info["is_windows"]:
-        return os.path.join(config_dir,"windows_config.json")
+        return os.path.join(config_dir,f"windows_{config_type}.json")
     elif plat_info["is_linux"]:
-        return os.path.join(config_dir,"linux_config.json")
+        return os.path.join(config_dir,f"linux_{config_type}.json")
     else:
-        return os.path.join(config_dir,"config_config.json")
+        return os.path.join(config_dir,f"default_{config_type}.json")
+
+def clear_screen():
+    """跨平台清屏"""
+    plat_info = get_paltform_info()
+    if plat_info["is_windows"]:
+        os.system("cls")
+    if plat_info["is_linux"]:
+        os.system("clear")
+
+def open_file_in_explorer(filepath):
+    """在文件管理器中打开文件"""
+    plat_info = get_paltform_info()
+    directory = os.path.dirname(filepath)
+    if plat_info["is_windows"]:
+        os.startfile(directory)
+    elif plat_info["is_linux"]:
+        os.system(f'xdg-open "{directory}"')
+    elif plat_info["is_mac"]:
+        os.system(f'open "{directory}"') #macOS使用open命令
+    else:
+        print(f'无法打开目录:{directory}')
